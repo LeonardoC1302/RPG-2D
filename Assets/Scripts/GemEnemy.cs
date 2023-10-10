dde21df68b202d418b87a6a324e6835bceaed41f
Kevin Chang <kchang@estudiantec.cr>
2023-10-09 21:26:59 -0600

    Animacion de Enemigo1
    Cuando lo toca hace su animacion

---
+++ Assets/Scripts/GemEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemEnemy : MonoBehaviour
+{
+    private Animator enemyAnimator;
+
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            if (enemyAnimator != null){
+                enemyAnimator.SetBool("Touch", true);
+            }
+        }
+    }
+
+    public void endAnimation(){
+        enemyAnimator.SetBool("Touch", false);;
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+}
