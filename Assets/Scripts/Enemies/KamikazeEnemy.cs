b7a364d7bd850af96481d136404a4329da368b0d
Leonardo Céspedes <lecespedes@estudiantec.cr>
2023-10-17 12:13:08 -0600

    Golf Speed Fix

--- Assets/Scripts/Enemies/KamikazeEnemy.cs
+++ Assets/Scripts/Enemies/KamikazeEnemy.cs
@@ -14,6 +14,7 @@ public class KamikazeEnemy : Enemy
             move();
         }else{
             if(!isExploding){
+                speed=0;
                 Explode();
             }
         }
